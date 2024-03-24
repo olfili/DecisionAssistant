f34173ea9cfa6ce425d860caf881b016b1b65ee7
LUKOVICHKA\filip <filipova.olga1990@gmail.com>
2024-03-24 17:45:31 +0100

    add assesment

--- DecisionPointsAssessments.xaml.cs
+++ DecisionPointsAssessments.xaml.cs
@@ -1,11 +1,37 @@
 using DecisionAssistant.Model;
+using System.Collections.ObjectModel;
+using System.Data;
 
 namespace DecisionAssistant;
 
 public partial class DecisionPointsAssessments : ContentPage
 {
-	public DecisionPointsAssessments(List<DecisionOption> decisionoptions)
+    public ObservableCollection<DecisionPointAssessment> Assessments { get; set; } = new ObservableCollection<DecisionPointAssessment>();
+    public List<DecisionOption> decisionOptions = new List<DecisionOption>();
+    private string decisionPoint = string.Empty;
+
+    public DecisionPointsAssessments(string decisionPoint, List<DecisionOption> decisionOptions, List<DecisionPoint> decisionPoints)
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+        this.decisionPoint = decisionPoint;
+        DecisionPoint.Text = decisionPoint;
+        this.decisionOptions = decisionOptions;
+
+        foreach (var point in this.AssemblePoints(decisionOptions, decisionPoints))
+        {
+            this.Assessments.Add(point);
+        }
+    }
+
+    private List<DecisionPointAssessment> AssemblePoints(List<DecisionOption> decisionOptions, List<DecisionPoint> decisionPoints)
+    {
+        var result = new List<DecisionPointAssessment>();
+        foreach (var option in decisionOptions)
+        {
+            result.Add(new DecisionPointAssessment(decisionPoints, option));
+        }
+
+        return result;
+    }
 }
--- DecisionPointsPage.xaml.cs
+++ DecisionPointsPage.xaml.cs
@@ -1,4 +1,5 @@
 using DecisionAssistant.Model;
+using MauiIcons.Core;
 using System.Collections.ObjectModel;
 
 namespace DecisionAssistant;
@@ -6,27 +7,53 @@ namespace DecisionAssistant;
 public partial class DecisionPointsPage : ContentPage
 {
     public ObservableCollection<DecisionPoint> DecisionPoints { get; set; } = new ObservableCollection<DecisionPoint>();
+    public List<DecisionOption> decisionOptions { get; set; } = new List<DecisionOption>();
     private string decisionPointName = string.Empty;
-    
-	public DecisionPointsPage(string decisionPoint, List<DecisionOption> decisionOptions)
+    private string decisionPoint = string.Empty;
+
+    public DecisionPointsPage(string decisionPoint, List<DecisionOption> decisionOptions)
 	{
 		InitializeComponent();
+        _ = new MauiIcon();
+        this.decisionPoint = decisionPoint;
+        this.decisionOptions = decisionOptions;
         BindingContext = this;
         DecisionPoint.Text = decisionPoint;
+        SubmitBtn.IsEnabled = IsSubmitEnabled();
+        AddBtn.IsEnabled = IsAddEnabled();
     }
 
     private void OnAddClicked(object sender, EventArgs e)
     {
         DecisionPoints.Add(new DecisionPoint(decisionPointName, 0));
+        SubmitBtn.IsEnabled = IsSubmitEnabled();
+        decisionPointName = string.Empty;
+        SubmitEntry.Text = string.Empty;
     }
 
     private void OnSubmitClicked(object sender, EventArgs e)
     {
-       // App.Current.MainPage = new NavigationPage(new DecisionPointsAssessments(DecisionPoints.ToList()));
+       App.Current.MainPage = new NavigationPage(new DecisionPointsAssessments(this.decisionPoint ,this.decisionOptions, DecisionPoints.ToList()));
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        DecisionPoints.Remove((DecisionPoint)((Button)sender).BindingContext);
     }
 
     void OnTextChanged(object sender, TextChangedEventArgs e)
     {
         decisionPointName = e.NewTextValue;
+        AddBtn.IsEnabled = IsAddEnabled();
+    }
+
+    bool IsSubmitEnabled()
+    {
+        return DecisionPoints.Count > 1 ? true : false;
+    }
+
+    bool IsAddEnabled()
+    {
+        return string.IsNullOrEmpty(decisionPointName) ? false : true;
     }
 }
